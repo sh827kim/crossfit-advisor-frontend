c4c325f06bef0999717bd4190ae69847e43362c7
spark <22386965+sh827kim@users.noreply.github.com>
2026-02-11 20:40:31 +0900

    fix: 레이아웃 수정, 앱 이름 수정

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.spark.afterwod',
-  appName: 'AfterWOD',
+  appName: '애프터와드',
   webDir: 'public',
   server: {
     url: 'https://afterwod.vercel.app',
